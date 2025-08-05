7c20175471862b43a87abc0781034e0901536971
vaggani-purushotham <purushothamvaggani@gmail.com>
2025-08-05 22:14:32 +0530

    My second commit

---
+++ eclipse-workspace/TNS FOUNDATION/src/Myproject/Myproject.java
@@ -0,0 +1,12 @@
+package Myproject;
+
+public class Myproject {
+	public void print() {
+        System.out.println("Welcome to Registration!");
+    }
+
+    public static void main(String[] args) {
+        Myproject db = new Myproject();
+        db.print(); 
+    }
+}
