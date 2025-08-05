91d83150fcf16316958c9425bf9a1fc0203f447c
vaggani-purushotham <purushothamvaggani@gmail.com>
2025-08-05 22:51:04 +0530

    My commit

---
+++ eclipse-workspace/Myfirstprogram/src/Myprogram/Myprogram.java
@@ -0,0 +1,13 @@
+package Myprogram;
+
+public class Myprogram {
+	public void print() {
+        System.out.println("Welcome to Registration!");
+    }
+
+    public static void main(String[] args) {
+        Myprogram db = new Myprogram();
+        db.print(); 
+    }
+}
+
